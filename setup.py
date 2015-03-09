c18a3d6a977983c241be7714c2da30ce5a28c80e
kscottz <katherineAScott@gmail.com>
2015-03-08 20:36:43 -0700

    working on a joystick controller.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['owi_arm'],
     package_dir={'': 'src'},
-    requires=['rospy','./scripts/owi_node.py']
+    requires=['rospy','./scripts/cmd_robot.py']
 )
 
 setup(**setup_args)
