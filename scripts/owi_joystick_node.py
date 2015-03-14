40ff4290a28a8a28831df5bf928a84f185927484
kscottz <katherineAScott@gmail.com>
2015-03-14 16:09:56 -0700

    this is sorta working.

--- scripts/owi_joystick_node.py
+++ scripts/owi_joystick_node.py
@@ -1,5 +1,6 @@
 import rospy
 import roscpp
+import numpy as np
 from sensor_msgs.msg import Joy
 from std_msgs.msg import Int16MultiArray
 class MyJoystickNode(object):
@@ -7,34 +8,44 @@ class MyJoystickNode(object):
          rospy.on_shutdown(self.shutdown)
          self.pub = rospy.Publisher('robot', Int16MultiArray, queue_size=1)
          rospy.Subscriber("/joy", Joy, self.do_it)
+         rospy.Subscriber("/state", Int16MultiArray, self.update_state)
          rospy.init_node('owi_joystick_node')
-         self.state = [0,0,0,0]
+         self.state = [0,0,0,0,0]
          rospy.spin()
-        
+
+    def update_state(self,msg):
+        self.state = msg.data
+#        rospy.loginfo(self.state)
+
     def do_it(self,msg):
-        s = 1000
-        m1 = 0
-        m2 = 0
-        m3 = 0
-        m4 = 0
+        print "hi"
+        m1 = self.state[1]
+        m2 = self.state[2]
+        m3 = self.state[3]
+        m4 = self.state[4]
+        step = 10
         if(msg.buttons[1] == 1 ):
-            m1 = s
+            m1+=step
         elif( msg.buttons[2] == 1 ):
-            m1 = -1*s
+            m1-=step
         if(msg.buttons[0] == 1 ):
-            m2 = s
-        elif( msg.buttons[3] == 1 ):
-            m2 = -1*s
+            m2+=step
+        elif( msg.buttons[3] ==1 ):
+            m2-=step
         if(msg.axes[-1] > 0 ):
-            m3 = s
+            m3+=step
         elif( msg.axes[-1] < 0 ):
-            m3 = -1*s
+            m3-=step
         if(msg.axes[-2] > 0 ):
-            m4 = s
+            m4+=step
         elif( msg.axes[-2] < 0 ):
-            m4 = -1*s
+            m4-=step
             
-        data = [int(m1),int(m2),int(m3),int(m4)]
+        data = [self.state[0],
+                int(np.clip(m1,0,180)),
+                int(np.clip(m2,0,180)),
+                int(np.clip(m3,0,180)),
+                int(np.clip(m4,0,180))]
         change = any([abs(a-b)>0 for a,b in zip(data,self.state)])
         self.state = data
         if( change ):
